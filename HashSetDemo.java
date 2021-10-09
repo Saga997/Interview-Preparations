f1e16be6df24246ad1c6a37a2777bc2f8f44b7fe
Jahid Quraishi <zaidism7@gmail.com>
2021-10-09 16:49:44 +0000

    HashSet

---
+++ HashSetDemo.java
@@ -0,0 +1,72 @@
+package com.collection.illustration;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+
+		Set<String> colors = new HashSet<String>();
+
+		colors.add("Green");
+		colors.add("Red");
+		colors.add("Blue");
+		colors.add("Black");
+		colors.add("Green");
+		colors.add("Orange");
+		colors.add("Pink");
+
+		colors.add(null);
+		System.out.println(colors);
+
+		// duplicates are not allowed
+		System.out.println("-------without duplicates--------------");
+		colors.forEach(e -> System.out.println(e));
+
+		// can allow null value
+		System.out.println("------can allow more than one null value-------------");
+		colors.add(null);
+		colors.forEach(e -> System.out.println(e));
+
+		// cannot allow more than one null value
+		colors.add(null);
+		System.out.println("-------cannot allow more than one null value----------");
+		colors.forEach(e -> System.out.println(e));
+
+		// order is not preserverd i.e. Set is unordered collection
+		System.out.println("order is not preserved: " + colors);
+
+		List<String> colorsList = new ArrayList<String>();
+		colorsList.add("Purple");
+		colorsList.add("Purple");
+		colorsList.add("Magenta Pink");
+		colorsList.add("Cyan");
+		colorsList.add("Burgundy");
+		colorsList.add("Grey");
+		colorsList.add("Grey");
+		System.out.println("get(0) of list: "+colorsList.get(0));
+		
+		colors.addAll(colorsList);
+		System.out.println("After adding ArrayList: " + colors);
+
+		Set<String> programmingLanguages = new LinkedHashSet<String>();
+
+		programmingLanguages.add("Java");
+		programmingLanguages.add("JavaScript");
+		programmingLanguages.add("C");
+		programmingLanguages.add("C#");
+		programmingLanguages.add("C++");
+		System.out.println("LinkedHasSet maintains the insertion order as it is an ordered collection : " + programmingLanguages);
+		
+		//remove
+		programmingLanguages.remove("Java");
+		programmingLanguages.remove("JavaScript");
+		System.out.println("after removal: "+programmingLanguages);
+		
+	}
+
+}
